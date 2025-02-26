63350ef96aad0ef18f029949171962a05290769e
Cauã Rodrigues <159566297+CauaRodrigues08@users.noreply.github.com>
2025-02-26 15:36:05 -0300

    Update project files.
    Tela de conteúdo quase completa; sem persistência de dados.

--- artefatos/arquivos-do-projeto/CoStudyOficial/CoStudyOficial/ConteudoView.swift
+++ artefatos/arquivos-do-projeto/CoStudyOficial/CoStudyOficial/ConteudoView.swift
@@ -8,9 +8,43 @@ import SwiftUI
 
 struct ConteudoView: View {
     
-    var conteudo : Conteudo
+    var conteudo: Conteudo
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var paginaDeTexto: String
+    init(conteudo: Conteudo) {
+        self.conteudo = conteudo
+        _paginaDeTexto = State(initialValue: conteudo.paginaDeTexto)
+    }
     
     var body: some View {
-        Text("Hello world")
+        ZStack {
+            //"Filtro" amarelo
+            Color(hex: "FFFBEF")
+                .ignoresSafeArea(.all)
+                .allowsHitTesting(false)
+            VStack {
+                ScrollView{
+                    TextField("Digite aqui...", text: $paginaDeTexto, axis: .vertical)
+                }
+            }
+            .frame(alignment: .topLeading)
+            .padding(16)
+            .navigationBarBackButtonHidden(true)
+            .navigationTitle(conteudo.nome)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: { dismiss() }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                                .foregroundStyle(Color(hex: "00504C"))
+                            Text("Conteúdos")
+                                .tint(Color(hex: "00504C"))
+                        }
+                    }
+                }
+            }
+        }
     }
 }
